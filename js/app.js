6b23775f8e8aec1839613e33525640eae4b5984b
devenock <enockomondi305@gmail.com>
2020-11-18 04:00:57 +0300

    Adding the toggle funtionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-// TOGGLE FUNCTIONALITY
-const toggleButton = document.querySelector('.toggle');
-const navbar = document.querySelector('.sidenav-menu');
-
-toggleButton.addEventListener('click', showNavbar);
-
-function showNavbar() {
-    if (navbar.style.visibility === "hidden") {
-        navbar.style.visibility = "visible";
-    } else {
-        navbar.style.visibility = "hidden";
-    }
-}
-// END OF TOGGLE FUNCTIONALITY
 
 const bar = document.getElementById('bar');
 bar.addEventListener('click', showNav);
