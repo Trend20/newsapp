9c0fd49e479127206f3c109969088f92b4271fb4
devenock <enockomondi305@gmail.com>
2020-11-18 11:16:02 +0300

    adding DOM selections

--- js/app.js
+++ js/app.js
@@ -12,3 +12,21 @@ function showNav() {
     }
 
 }
+
+// DOM SELECTIONS
+const exploreButton = document.getElementById('explore');
+
+// like and unlike buttons
+
+const likeButton = document.querySelectorAll('#like-button');
+const dislikeButton = document.querySelectorAll('#dislike-button');
+
+
+// search through the page
+// search input field
+const searchField = document.getElementById('search');
+// grab the search icon
+const searcIcon = document.getElementById('search-icon');
+
+// select the whole card
+const card = document.querySelectorAll('.card');
